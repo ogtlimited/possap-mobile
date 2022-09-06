129aef5917259595cbd73f99cb581bbf639649f4
Sir Abubakar <60545690+Abubakar-Abdulwahab@users.noreply.github.com>
2022-09-06 10:31:07 +0100

    removed api key

--- src/app/LanguageLoader.ts
+++ src/app/LanguageLoader.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
 export function languageLoader(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
 }
